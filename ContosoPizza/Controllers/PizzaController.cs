0315bcbbc11fd886258921af6bb63b14dc424ac9
misiekroz <56201616+misiekroz@users.noreply.github.com>
2024-04-03 18:27:27 +0200

    feat (PizzaController): POST PUT and DELETE endpoints added and tested with .http

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -28,9 +28,38 @@ public class PizzaController : ControllerBase
         return pizza;
     }
 
-    // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.ID }, pizza);
+    }
 
-    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.ID)
+            return BadRequest();
 
-    // DELETE action
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
