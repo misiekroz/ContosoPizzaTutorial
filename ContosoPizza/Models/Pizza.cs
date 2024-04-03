f531e35b6fb1e5724c2a38f7ee98d9a9904eb006
misiekroz <56201616+misiekroz@users.noreply.github.com>
2024-04-03 17:55:38 +0200

    feat: pizza model created

---
+++ ContosoPizza/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace ContosoPizza.Models
+{
+    public class Pizza
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
