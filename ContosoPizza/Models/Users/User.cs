672bf196dd80bbead5de465b66f365b022edd059
misiekroz <56201616+misiekroz@users.noreply.github.com>
2024-04-05 17:00:13 +0200

    feat: implemented User class with basic user information

---
+++ ContosoPizza/Models/Users/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ContosoPizza.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public DateTime LastOrder { get; set; } = default;
+
+        public int OrdersCount { get; set; }
+
+    }
+}
