d78b3c74bf9781ea334cf305ba7210b2313ed09c
misiekroz <56201616+misiekroz@users.noreply.github.com>
2024-04-08 06:53:36 +0200

    feat: added a model for Order

---
+++ ContosoPizza/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoPizza.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+
+        public int UserID { get; set; }
+
+        public int PremiumPoints { get; set; }
+    }
+}
