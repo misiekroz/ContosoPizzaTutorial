a0bc696557f9dd88bf58701fc3c2aa338d6a2d94
misiekroz <56201616+misiekroz@users.noreply.github.com>
2024-04-09 16:22:43 +0200

    feat: created a universal ControsoService implementing an IContosoService interface

---
+++ ContosoPizza/Services/ContosoService.cs
@@ -0,0 +1,43 @@
+﻿using ContosoPizza.Models;
+using System.Xml.Linq;
+
+namespace ContosoPizza.Services;
+
+public class ContosoService : IContosoService<IContosoModel>
+{
+    List<IContosoModel> Models { get; } = [];
+    int nextId = 1;
+    ContosoService()
+    {
+    }
+
+    public List<IContosoModel> GetAll() => Models;
+
+    public IContosoModel? Get(int id) => Models.FirstOrDefault<IContosoModel>(p => p.ID == id);
+
+    public void Add(IContosoModel item)
+    {
+        item.ID = nextId++;
+        Models.Add(item);
+    }
+
+    public void Delete(int id)
+    {
+        var item = Get(id);
+        if (item is null)
+            return;
+
+        Models.Remove(item);
+    }
+
+    public bool Update(IContosoModel item)
+    {
+        var index = Models.FindIndex(p => p.ID == item.ID);
+        if (index == -1)
+            return false;
+
+        Models[index] = item;
+
+        return true;
+    }
+}
