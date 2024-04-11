e3e26a3415f5ad77eabc30126d7a17fbbbfad6c0
misiekroz <56201616+misiekroz@users.noreply.github.com>
2024-04-11 14:58:52 +0200

    feat: premium user controller and updated PremiumUser model

---
+++ ContosoPizza/Controllers/PremiumUserController.cs
@@ -0,0 +1,85 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Models.Users;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PremiumUserController : ControllerBase
+{
+    IContosoService<PremiumUser> premiumUserService;
+    IContosoService<User> userService;
+
+    const int premiumTreshold = 10;
+
+    public PremiumUserController(IContosoService<PremiumUser> premiumUserService, IContosoService<User> userService)
+    {
+        this.premiumUserService = premiumUserService;
+        this.userService = userService;
+    }
+
+    [HttpGet]
+    public ActionResult<List<PremiumUser>> GetAll() => premiumUserService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<PremiumUser> Get(int id)
+    {
+        var User = premiumUserService.Get(id);
+
+        if (User == null)
+        {
+            return NotFound();
+        }
+
+        return User;
+    }
+
+    [HttpPost]
+    public IActionResult Create(int currentUserId)
+    {
+        var currentUser = userService.Get(currentUserId);
+        if (currentUser is null)
+            return NotFound();
+
+        // if user made enough orders, proceed with account type switch
+        if (currentUser.OrdersCount < premiumTreshold)
+            return BadRequest($"User {currentUserId} did not make enough orders.");
+
+        // remove the user from db
+        userService.Delete(currentUserId);
+        // add the user to premium users db
+        var newUser = new PremiumUser(currentUser);
+        premiumUserService.Add(newUser);
+        return CreatedAtAction(nameof(Get), new { id = currentUserId }, newUser);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, PremiumUser user)
+    {
+        if (id != user.ID)
+            return BadRequest("ID cannot be changed");
+
+        var existingUser = premiumUserService.Get(id);
+        if (existingUser is null)
+            return NotFound();
+
+        premiumUserService.Update(user);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var User = premiumUserService.Get(id);
+
+        if (User is null)
+            return NotFound();
+
+        premiumUserService.Delete(id);
+
+        return NoContent();
+    }
+}
--- ContosoPizza/Models/Users/PremiumUser.cs
+++ ContosoPizza/Models/Users/PremiumUser.cs
@@ -4,5 +4,15 @@
     {
         public int Points { get; set; }
         public DateTime JoinedTime { get; set; } = default;
+
+        public PremiumUser(User baseUser)
+        {
+            LastOrder = baseUser.LastOrder;
+            Address = baseUser.Address;
+            City = baseUser.City;
+            Email = baseUser.Email;
+            Name = baseUser.Name;
+            OrdersCount = baseUser.OrdersCount;
+        }
     }
 }
